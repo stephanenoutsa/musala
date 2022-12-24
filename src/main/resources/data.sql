5b55a396a88ff54936604d40e5bd8965df25810f
Stéphane Noutsa <stephanenoutsak@gmail.com>
2022-12-25 00:08:34 +0100

    Preloads a few medications

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,10 +1,16 @@
-INSERT INTO drone (serial_number, model, weight_limit, battery_capacity, state) VALUES ('9751fb8e-82cf-11ed-a1eb-0242ac120002', 'Lightweight', 500, 100, 'IDLE');
-INSERT INTO drone (serial_number, model, weight_limit, battery_capacity, state) VALUES ('97520322-82cf-11ed-a1eb-0242ac120002', 'Middleweight', 500, 100, 'IDLE');
-INSERT INTO drone (serial_number, model, weight_limit, battery_capacity, state) VALUES ('9752049e-82cf-11ed-a1eb-0242ac120002', 'Cruiserweight', 500, 100, 'IDLE');
+INSERT INTO drone (serial_number, model, weight_limit, battery_capacity, state) VALUES ('9751fb8e-82cf-11ed-a1eb-0242ac120002', 'Lightweight', 100, 25, 'IDLE');
+INSERT INTO drone (serial_number, model, weight_limit, battery_capacity, state) VALUES ('97520322-82cf-11ed-a1eb-0242ac120002', 'Middleweight', 150, 100, 'IDLE');
+INSERT INTO drone (serial_number, model, weight_limit, battery_capacity, state) VALUES ('9752049e-82cf-11ed-a1eb-0242ac120002', 'Cruiserweight', 200, 100, 'IDLE');
 INSERT INTO drone (serial_number, model, weight_limit, battery_capacity, state) VALUES ('97520688-82cf-11ed-a1eb-0242ac120002', 'Heavyweight', 500, 100, 'IDLE');
-INSERT INTO drone (serial_number, model, weight_limit, battery_capacity, state) VALUES ('975207be-82cf-11ed-a1eb-0242ac120002', 'Lightweight', 500, 100, 'IDLE');
-INSERT INTO drone (serial_number, model, weight_limit, battery_capacity, state) VALUES ('975208d6-82cf-11ed-a1eb-0242ac120002', 'Middleweight', 500, 100, 'IDLE');
-INSERT INTO drone (serial_number, model, weight_limit, battery_capacity, state) VALUES ('97520a66-82cf-11ed-a1eb-0242ac120002', 'Cruiserweight', 500, 100, 'IDLE');
-INSERT INTO drone (serial_number, model, weight_limit, battery_capacity, state) VALUES ('97520bce-82cf-11ed-a1eb-0242ac120002', 'Heavyweight', 500, 100, 'IDLE');
-INSERT INTO drone (serial_number, model, weight_limit, battery_capacity, state) VALUES ('97520cf0-82cf-11ed-a1eb-0242ac120002', 'Lightweight', 500, 100, 'IDLE');
+INSERT INTO drone (serial_number, model, weight_limit, battery_capacity, state) VALUES ('975207be-82cf-11ed-a1eb-0242ac120002', 'Lightweight', 250, 100, 'IDLE');
+INSERT INTO drone (serial_number, model, weight_limit, battery_capacity, state) VALUES ('975208d6-82cf-11ed-a1eb-0242ac120002', 'Middleweight', 300, 100, 'IDLE');
+INSERT INTO drone (serial_number, model, weight_limit, battery_capacity, state) VALUES ('97520a66-82cf-11ed-a1eb-0242ac120002', 'Cruiserweight', 350, 100, 'IDLE');
+INSERT INTO drone (serial_number, model, weight_limit, battery_capacity, state) VALUES ('97520bce-82cf-11ed-a1eb-0242ac120002', 'Heavyweight', 400, 100, 'IDLE');
+INSERT INTO drone (serial_number, model, weight_limit, battery_capacity, state) VALUES ('97520cf0-82cf-11ed-a1eb-0242ac120002', 'Lightweight', 450, 100, 'IDLE');
 INSERT INTO drone (serial_number, model, weight_limit, battery_capacity, state) VALUES ('97521272-82cf-11ed-a1eb-0242ac120002', 'Middleweight', 500, 100, 'IDLE');
+
+INSERT INTO medication (code, name, weight, image) VALUES ('ABC_1', 'ABC_a-1', 20, '');
+INSERT INTO medication (code, name, weight, image) VALUES ('ABC_2', 'ABC_b-1', 30, '');
+INSERT INTO medication (code, name, weight, image) VALUES ('ABC_3', 'ABC_c-1', 40, '');
+INSERT INTO medication (code, name, weight, image) VALUES ('ABC_4', 'ABC_d-1', 50, '');
+INSERT INTO medication (code, name, weight, image) VALUES ('ABC_5', 'ABC_e-1', 60, '');
